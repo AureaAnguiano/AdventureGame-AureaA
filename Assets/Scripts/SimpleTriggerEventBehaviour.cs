0fdfd886b0528c76e38db94bfc92e4732a41f8e4
Aurea Anguiano <11057966@uvu.edu>
2025-03-19 01:44:10 -0600

    healthbar

--- Assets/Scripts/SimpleTriggerEventBehaviour.cs
+++ Assets/Scripts/SimpleTriggerEventBehaviour.cs
@@ -12,15 +12,5 @@ public class SimpleTriggerEventBehaviour : MonoBehaviour
         triggerEvent.Invoke();
         Debug.Log("Player interacted with the object!");
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
 }
